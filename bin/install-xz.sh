60fd5adbb49908ef7859a26851c870887c9ef80e
Łukasz Wieczorek <luke@soiree.tech>
2026-07-25 22:23:21 +0200

    Add prefix.

--- bin/install-xz.sh
+++ bin/install-xz.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 # Installer for XZ utilities.
 
+source ./bin/prefix.sh
+
 curl --location --output xz-5.8.3.tar.gz https://github.com/tukaani-project/xz/releases/download/v5.8.3/xz-5.8.3.tar.gz
 tar xfz xz-5.8.3.tar.gz
 cd xz-5.8.3/
 
-./configure
+./configure --prefix=$prefix
 make
 sudo make install
