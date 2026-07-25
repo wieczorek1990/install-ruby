9551a92e9594ea50b2b14a4a0ea24d8f112b95e5
Łukasz Wieczorek <luke@soiree.tech>
2026-07-25 22:25:20 +0200

    Add prefix.

--- bin/install-yaml.sh
+++ bin/install-yaml.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 # YAML instaler.
 
+source ./bin/prefix.sh
+
 curl --location --output yaml-0.2.5.tar.gz http://pyyaml.org/download/libyaml/yaml-0.2.5.tar.gz
 tar xfz yaml-0.2.5.tar.gz
 cd yaml-0.2.5/
 
-./configure
+./configure --prefix=$prefix
 make
 sudo make install
