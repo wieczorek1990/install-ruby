ad42d7cdae19b97924568e4b272825ec84097cdf
Łukasz Wieczorek <luke@soiree.tech>
2026-07-25 21:55:28 +0200

    Update installers.

--- bin/install-openssl.sh
+++ bin/install-openssl.sh
@@ -10,9 +10,9 @@ then
 else
   curl --location --output openssl-4.0.1.tar.gz https://github.com/openssl/openssl/archive/refs/tags/openssl-4.0.1.tar.gz
 fi
-
 tar xzf openssl-4.0.1.tar.gz
 cd openssl-openssl-4.0.1/
+
 ./Configure --prefix=$prefix --release
 make
 sudo make install
--- bin/install-psych.sh
+++ bin/install-psych.sh
@@ -3,6 +3,5 @@
 
 source ./bin/prefix.sh
 
-# Psych
 LDFLAGS=$prefix/lib CFLAGS=$prefix/include\
  sudo gem install psych -- --with-libyaml-dir=$prefix
